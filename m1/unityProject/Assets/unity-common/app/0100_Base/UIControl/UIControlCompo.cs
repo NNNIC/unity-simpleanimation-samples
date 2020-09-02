96b9b9156551a0889e7cccd6ad8575185ed8afec
nn4343 <gea01441@nifty.com>
2020-09-02 10:37:01 +0900

    Unity 2020 対応

--- m1/unityProject/Assets/unity-common/app/0100_Base/UIControl/UIControlCompo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UIControlCompo : MonoBehaviour {
-    public virtual void SetTarget_TemplateAndStart() { }
-    public virtual bool IsEnd() { return true;}
-}
--- m2/startProject.bat
+++ m2/startProject.bat
@@ -2,10 +2,10 @@ cd /d %~dp0
 
 if exist %~dp0unityProject goto :_open
 
-start "" "%unitypath2018_2%" -createproject %~dp0unityProject
+start "" "%unitypath2020%" -createproject %~dp0unityProject
 goto :eof
 
 :_open
 
-start "" "%unitypath2018_2%" -projectPath %~dp0unityProject
+start "" "%unitypath2020%" -projectPath %~dp0unityProject
 goto :eof
